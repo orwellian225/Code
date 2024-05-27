5a2d6d9c70a57a0c6737a73f38aa366fc4bb73b9
Orwellian <orwellian225@gmail.com>
2024-05-27 23:33:44 +0200

    modify: graph class   fix: Graph visualization   add: graph connectivity sequence   add: graph edge sequence   add: graph permutations

--- classical/graph.py
+++ classical/graph.py
@@ -11,8 +11,38 @@ class Graph:
         self.adj_matrix = adjcency_matrix
         self.num_vertices = len(self.adj_matrix)
 
+        np.fill_diagonal(self.adj_matrix, 0)
+
     def __eq__(self, value: object) -> bool:
         return self.adj_matrix == value.adj_matrix
+        
+    def __str__(self):
+        return self.to_bitstring()
+
+    def get_edge_sequence(self) -> np.ndarray:
+        return np.sum(self.adj_matrix, axis=1)
+
+    def get_connectivity_sequence(self) -> np.ndarray:
+        sequence = np.zeros(self.num_vertices)
+
+        for i in range(self.num_vertices):
+            for j in range(i + 1, self.num_vertices):
+                binary_idx = self.num_vertices - j - 1 
+                sequence[i] += self.adj_matrix[i,j] * 2**binary_idx
+
+        return sequence
+
+    def permute(self, vertex_1, vertex_2) -> None:
+        self.adj_matrix[[vertex_1, vertex_2]] = self.adj_matrix[[vertex_2, vertex_1]]
+        self.adj_matrix = np.transpose(self.adj_matrix)
+        self.adj_matrix[[vertex_1, vertex_2]] = self.adj_matrix[[vertex_2, vertex_1]]
+        self.adj_matrix = np.transpose(self.adj_matrix)
+
+    def copy_permute(self, vertex_1, vertex_2):
+        copy = Graph(np.copy(self.adj_matrix))
+        copy.permute(vertex_1, vertex_2)
+        return copy
+
 
     def to_bitstring(self) -> str:
         mask = np.triu(np.ones(self.adj_matrix.shape), k=1)
@@ -20,18 +50,25 @@ class Graph:
         result = "".join(map(str, elements))
         return result
 
-    def plot_graph(self) -> None:
-        # plt.scatter(np.arange(0, 5), np.arange(5, 10))
+    def plot_graph(self, vertex_labels: None | bool) -> None:
+        plt.figure(figsize=(5,5))
 
         radius = 5
         positions_x = list(map(lambda x: radius * m.cos(x * 2 * m.pi / self.num_vertices), np.arange(0, self.num_vertices)))
         positions_y = list(map(lambda x: radius * m.sin(x * 2 * m.pi / self.num_vertices), np.arange(0, self.num_vertices)))
 
+        if vertex_labels is not None and vertex_labels:
+            for i in range(self.num_vertices):
+                plt.text(positions_x[i], positions_y[i], str(i))
+
         for i in range(self.num_vertices):
             for j in range(self.num_vertices):
-                if self.adj_matrix[i, j] == 1:
-                    plt.plot([positions_x[i], positions_y[i]], [positions_x[j], positions_y[j]], color="b")
+                if self.adj_matrix[i,j] == 1:
+                    plt.plot(
+                        (positions_x[i], positions_x[j]),
+                        (positions_y[i], positions_y[j]),
+                        color='blue'
+                    )
 
         plt.scatter(positions_x, positions_y, c='r')
-
         plt.show()
